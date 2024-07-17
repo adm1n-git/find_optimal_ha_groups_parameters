e2f36632a7c70e43ba9b5b7f221ebbbcb2ac3b4d
Ayyappan Mani <ayyappan.mani@outlook.in>
2024-07-17 17:56:35 +0000

    ...

--- find_optimal_ha_groups_parameters.py
+++ find_optimal_ha_groups_parameters.py
@@ -1,6 +1,6 @@
 
 def validate_optimal_ha_groups_parameters(active_bonus, number_of_members, sufficient_member_count, weight, lb1="active", lb2="standby"):
-    print("\nValidation:");
+    print("\nValidation Results:");
     for lb1_active_member_count in range(number_of_members,-1,-1):
         for lb2_active_member_count in range(number_of_members,-1,-1):
             if lb1_active_member_count >= sufficient_member_count:
@@ -10,19 +10,19 @@ def validate_optimal_ha_groups_parameters(active_bonus, number_of_members, suffi
                     lb1_score = weight;
             else:
                 if "active" in lb1:
-                    lb1_score = active_bonus + (weight*(lb1_active_member_count/sufficient_member_count));
+                    lb1_score = active_bonus + round(weight*(lb1_active_member_count/sufficient_member_count));
                 else:
-                    lb1_score = weight*(lb1_active_member_count/sufficient_member_count);            
+                    lb1_score = round(weight*(lb1_active_member_count/sufficient_member_count));            
             if lb2_active_member_count >= sufficient_member_count:
                 if "active" in lb2:
-                    lb2_score = (active_bonus - 10) + weight;
+                    lb2_score = active_bonus + weight;
                 else:
                     lb2_score = weight;
             else:
                 if "active" in lb2:
-                    lb2_score = (active_bonus - 10) + (weight*(lb2_active_member_count/sufficient_member_count));
+                    lb2_score = active_bonus + round(weight*(lb2_active_member_count/sufficient_member_count));
                 else:
-                    lb2_score = weight*(lb2_active_member_count/sufficient_member_count);
+                    lb2_score = round(weight*(lb2_active_member_count/sufficient_member_count));
                     
             if lb1_score > lb2_score:
                 lb1, lb2 = "active", "standby";
@@ -34,8 +34,8 @@ def validate_optimal_ha_groups_parameters(active_bonus, number_of_members, suffi
                 print("[-] Weight is inappropriate.");
 
 def find_optimal_ha_groups_parameters():
-    active_bonus = 20;
-    number_of_members = int(input("[*] Provide number of port-channel members: "));
+    active_bonus = 1;
+    number_of_members = int(input("\n[*] Provide number of port-channel members: "));
     minimum_member_count = 1;
     if number_of_members > 1:
         sufficient_member_count = number_of_members//2;
@@ -54,7 +54,7 @@ def find_optimal_ha_groups_parameters():
                 break;
         else:
             print(f"\nLB1:\nActive Bonus: {active_bonus}, Number of Members: {number_of_members}, Minimum Member Count: {minimum_member_count}, Sufficient Member Count: {sufficient_member_count}, Weight: {weight}");
-            print(f"\nLB2:\nActive Bonus: {active_bonus-10}, Number of Members: {number_of_members}, Minimum Member Count: {minimum_member_count}, Sufficient Member Count: {sufficient_member_count}, Weight: {weight}");
+            print(f"LB2:\nActive Bonus: {active_bonus}, Number of Members: {number_of_members}, Minimum Member Count: {minimum_member_count}, Sufficient Member Count: {sufficient_member_count}, Weight: {weight}");
             validate_optimal_ha_groups_parameters(active_bonus, number_of_members, sufficient_member_count, weight);
             break;
 
